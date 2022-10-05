b999373b9514082b0f3286fd7f27add1f1f40f49
Andrei Dogadaev <dogadlev@yandex.ru>
2022-10-05 21:46:41 +0300

    Initial commit

---
+++ task_1/task_1.cpp
@@ -0,0 +1,25 @@
+﻿/* task_1.cpp : Лямбда-функции.*/
+
+#include <algorithm> /*use std::for_each()*/
+#include <vector> /*use std::vector<>*/
+#include <iostream>
+#include <locale.h> /*use setlocale(); need to output russian letters in console*/
+
+int main(int argc, char** argv)
+{
+    std::vector<int> numbers{4, 7, 9, 14, 12};
+    
+    setlocale(LC_ALL, "Russian");
+    
+    std::cout << "Входные данные: ";
+    std::for_each(numbers.begin(), numbers.end(), [](const int num) { std::cout << num << " "; });
+    std::cout << std::endl;
+    
+    std::for_each(numbers.begin(), numbers.end(), [](int& num) { if (num % 2 != 0) num *= 3; });
+    
+    std::cout << "Выходные данные: ";
+    std::for_each(numbers.begin(), numbers.end(), [](const int num) { std::cout << num << " "; });
+    std::cout << std::endl;
+    
+    return 0;
+}
---
+++ task_2/task_2.cpp
@@ -0,0 +1,53 @@
+﻿// task_2.cpp : std::variant.
+#include <algorithm>
+#include <variant>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iostream>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+void printResults(std::variant<int, std::string, std::vector<int>> &var) {
+	if (std::holds_alternative<int>(var)) {
+		std::cout << std::get<int>(var) * 2;
+	}
+	else if (std::holds_alternative<std::string>(var)) {
+		std::cout << std::get<std::string>(var);
+	}
+	else {
+		for (int num : std::get<std::vector<int>>(var)) {
+			std::cout << num << " ";
+		}
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	std::variant<int, std::string, std::vector<int>> var = get_variant();
+	printResults(var);
+	
+    return 0;
+}
